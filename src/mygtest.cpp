0588c40e9d4563afe4d2e18a05c9eed69de5ecb8
Jim Fan <jimfanspire@gmail.com>
2015-03-17 12:47:16 -0400

    Add threading to mygtest.cpp

--- src/mygtest.cpp
+++ src/mygtest.cpp
@@ -1,9 +1,19 @@
 #include "myutils.h"
+#include "mythreads.h"
 #include <gtest/gtest.h>
+#include <future>
 using namespace std;
+
+int sq(int i) { return i * i; }
 
 TEST(MyTestSuite, Dudulu)
 {
     EXPECT_EQ(1+2, 3) << "shitty";
     EXPECT_EQ(1+2, 4) << "shitty";
+}
+
+TEST(MyTestSuite, Async)
+{
+    future<int> res = async(sq, 13);
+    EXPECT_EQ(res.get(), 169) << "async test fail";
 }
