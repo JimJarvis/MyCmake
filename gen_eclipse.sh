9915ea167c6cc98c5272cd8e10ba719252e0fcf8
Jim Fan <jimfanspire@gmail.com>
2015-03-18 00:11:27 -0400

    NEW bash script that generates eclipse project dir

---
+++ gen_eclipse.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# arg1: give a build directory name. Defaults to 'project'
+
+BUILD_DIR=project
+if [ $# -eq 1 ]; then
+    BUILD_DIR=$1
+fi
+
+mkdir $BUILD_DIR
+cd $BUILD_DIR
+cmake -G "Eclipse CDT4 - Unix Makefiles" -DCMAKE_CXX_COMPILER_ARG1=-std=c++11 -DCMAKE_BUILD_TYPE=Release ../cmake/
+cd ..
+python patch_eclipse.py $BUILD_DIR
+
+echo
+echo DONE
