bd5fb9435f9235ba8245a396e4678b1bf07da627
Jim Fan <jimfanspire@gmail.com>
2015-03-21 14:15:24 -0400

    gen_eclipse will skip an existing build folder

--- gen_eclipse.sh
+++ gen_eclipse.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 # arg1: give a build directory name. Defaults to 'project'
 
-BUILD_DIR=project
+BUILD_DIR=ubuntu
+
 if [ $# -eq 1 ]; then
     BUILD_DIR=$1
 fi
 
-mkdir $BUILD_DIR
-cd $BUILD_DIR
-cmake -G "Eclipse CDT4 - Unix Makefiles" -DCMAKE_CXX_COMPILER_ARG1=-std=c++11 -DCMAKE_BUILD_TYPE=Release ../cmake/
-cd ..
+if [ ! -d $BUILD_DIR ]; then
+    mkdir $BUILD_DIR
+    cd $BUILD_DIR
+    cmake -G "Eclipse CDT4 - Unix Makefiles" -DCMAKE_CXX_COMPILER_ARG1=-std=c++11 -DCMAKE_BUILD_TYPE=Release ../cmake/
+    cd ..
+else
+    echo $BUILD_DIR already exists.
+fi
 
 echo
 echo Patch eclipse project configurations:
